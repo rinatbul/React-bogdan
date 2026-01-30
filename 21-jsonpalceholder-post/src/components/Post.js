d76f7afd9ee2b67070d3177a867973208d656391
@RBuldybayev <rbuldybayev@nurbank.kz>
2026-01-30 17:33:40 +0500

    posts

--- 21-jsonpalceholder-post/src/components/Post.js
+++ 21-jsonpalceholder-post/src/components/Post.js
@@ -1,5 +1,14 @@
-function Post(){
-    return <h1>This is Post</h1>
+import './Post.css'
+
+function Post({id,body,title, userId}) {
+  return (
+    <div className='post'>
+      <small>{id}</small>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <h3>User ID: {userId}</h3>
+    </div>
+  )
 }
 
 export default Post
--- 21-jsonpalceholder-post/src/components/Posts.js
+++ 21-jsonpalceholder-post/src/components/Posts.js
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react"
-import Post from "./Post"
+import { useEffect, useState } from 'react'
+import Post from './Post'
 
 function Posts() {
-    const [posts, setPosts] = useState([])
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(posts => {
-            console.log(posts);
-            setPosts(posts)
-        })
-    },[])
-    return (
-        <div>
-            <Post/>
-            <Post/>
-        </div>
-    )
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts))
+      .catch((error) => console.log(error.message))
+  }, [])
+  return (
+    <div>
+      {posts.map((post, key) => (
+        <Post {...post} key={post.id}/>
+      ))}
+    </div>
+  )
 }
 
 export default Posts
